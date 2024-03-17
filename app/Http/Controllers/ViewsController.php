54d1d1692c2ddc6c328b91c579028546a423fab8
Ola <olafarhat2@gmail.com>
2024-03-18 01:21:17 +0300

    add template

---
+++ app/Http/Controllers/ViewsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ViewsController extends Controller
+{
+    public function index(){
+        
+        
+        return view ('index');
+    }
+
+    public function about(){
+        
+       
+        return view ('about');
+    }
+    
+    public function contact(){
+       
+        return view ('contact');
+
+    }
+     
+    public function gallery(){
+        
+       
+        return view ('gallery');
+    }
+
+    public function product(){
+        
+        return view ('product');
+    }
+
+    public function service(){
+        
+        return view ('service');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ViewsController::class, 'index'])->name('index');
+Route::get('/about',[ViewsController::class, 'about'])->name('about');
+Route::get('/contact',[ViewsController::class, 'contact'])->name('contact');
+Route::get('/gallery',[ViewsController::class, 'gallery'])->name('gallery');
+Route::get('/product',[ViewsController::class, 'product'])->name('product');
+Route::get('/service',[ViewsController::class, 'service'])->name('service');
